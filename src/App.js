11075bdab7441a6f4aa5d3310ed55a439118a4e6
Udaykiran <uuday3804@gmail.com>
2022-01-29 10:33:08 +0000

    solution

--- src/App.js
+++ src/App.js
@@ -2,41 +2,33 @@ import {Component} from 'react'
 
 import './App.css'
 
-const usha = true
-
 class App extends Component {
   state = {
     search: '',
-    uday: [],
-  }
-
-  componentDidMount() {
-    localStorage.setItem('search1', '')
+    name: '',
+    ram: true,
   }
 
   getDetails = async () => {
-    const search1 = localStorage.getItem('search1')
-    console.log(search1)
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${search1}&appid=c3e89bc97eb6bd54a4b2b7ea34b98c35`
+    const {search} = this.state
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&appid=c3e89bc97eb6bd54a4b2b7ea34b98c35`
     const response = await fetch(url)
     const data = await response.json()
-    console.log(response.ok)
-
+    console.log(data)
     if (response.ok === false) {
-      localStorage.setItem('object', true)
+      localStorage.removeItem('ram')
+      this.setState({ram: true, name: '', search: ''})
+    } else {
+      localStorage.setItem('temp', data.main.temp)
+      localStorage.setItem('minTemp', data.main.temp_min)
+      localStorage.setItem('maxTemp', data.main.temp_max)
+      localStorage.setItem('weather', data.weather[0].main)
+      localStorage.setItem('ram', false)
+      localStorage.setItem('humidity', data.main.humidity)
+      localStorage.setItem('pressure', data.main.pressure)
+      localStorage.setItem('name', data.name)
+      this.setState({name: data.name})
     }
-    const ram = {
-      temp: data.main.temp,
-      minTemp: data.main.temp_min,
-      maxTemp: data.main.temp_max,
-      weather: data.weather[0].main,
-      ram: false,
-      humidity: data.main.humidity,
-      pressure: data.main.pressure,
-      name: data.name,
-    }
-    localStorage.setItem('object', JSON.stringify(ram))
-    this.setState({uday: ram})
   }
 
   searchCity = event => {
@@ -45,26 +37,21 @@ class App extends Component {
 
   press = event => {
     if (event.key === 'Enter') {
-      const {search} = this.state
-      localStorage.removeItem('object')
-      localStorage.setItem('search1', search)
       this.getDetails()
     }
   }
 
   render() {
-    const weatherData = localStorage.getItem('object')
-    const {
-      temp,
-      minTemp,
-      maxTemp,
-      weather,
-      ram,
-      humidity,
-      pressure,
-      name,
-    } = JSON.parse(weatherData)
-
+    const temp = localStorage.getItem('temp')
+    const minTemp = localStorage.getItem('minTemp')
+    const maxTemp = localStorage.getItem('maxTemp')
+    const weather = localStorage.getItem('weather')
+    const pressure = localStorage.getItem('pressure')
+    const name = localStorage.getItem('name')
+    const search = localStorage.getItem('search')
+    const humidity = localStorage.getItem('humidity')
+    const ram = localStorage.getItem('ram')
+    console.log(temp)
     return (
       <div className="background">
         <div className="background1">
@@ -75,13 +62,14 @@ class App extends Component {
             onKeyPress={this.press}
             placeholder="Enter the city name"
           />
-          <div className="uday">
-            <h1>{name}</h1>
-          </div>
-          {ram ? (
+
+          {ram === null ? (
             <h1>The Country Not Found</h1>
           ) : (
             <>
+              <div className="uday">
+                <h1>{name}</h1>
+              </div>
               <h1>{temp} F</h1>
               <h2>{weather}</h2>
               <p>Coldest Temperature: {minTemp} F </p>
